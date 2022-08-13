4f219ac049c6cba3aee0e31aa6a0ffd0bc17de18
Jhom <jhomwan238@gmail.com>
2022-08-13 20:29:42 +0700

    adding navigation paths

--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -1,24 +1,26 @@
-import classes from './Navigation.module.css'
+import { NavLink } from "react-router-dom";
+
+import classes from "./Navigation.module.css";
 
 const Navigation = () => {
-    return(
-        <header className={classes.header}>
-            <div className={classes.logo}>Logo</div>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        Random
-                    </li>
-                    <li>
-                        All Quotes
-                    </li>
-                    <li>
-                        + Add Quote
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Logo</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to="/">Random</NavLink>
+          </li>
+          <li>
+            <NavLink to="fav-quotes" activeClassName={classes.active}>Favorite Quotes</NavLink>
+          </li>
+          <li>
+            <NavLink to="add-quote" activeClassName={classes.active}>+ Add Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navigation;
---
+++ src/pages/FavoriteQuotes.js
@@ -0,0 +1,9 @@
+import Quotes from '../components/quotes/Quotes'
+
+const FavoriteQuotes = () => {
+    return(
+        <Quotes/>
+    );
+}
+
+export default FavoriteQuotes;
